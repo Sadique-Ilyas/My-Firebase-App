57be4b13cca2e805979fdf6fc4d8d5c13700718d
Sadique Ilyas <sadiqueilyasfifa@gmail.com>
2020-07-06 12:20:30 +0530

    d

--- app/src/main/java/com/example/myfirebaseapp/SignUpActivity.java
+++ app/src/main/java/com/example/myfirebaseapp/SignUpActivity.java
@@ -24,7 +24,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         signUpEmail = findViewById(R.id.signUpEmail);
